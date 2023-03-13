e32d8e16a4f959b2e294e629c4b34f3e612a70fc
Santu De <Santu.De@inindcrws.com>
2023-03-13 18:57:02 +0530

    usestate hook understanding with basic operations

--- src/components/UseStateHooks/UseStateHooks.js
+++ src/components/UseStateHooks/UseStateHooks.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const UseStateHooks = () => {
+    let number = 1;
+    const [num, setNum] = useState(number);
+
+    function inc(){
+        setNum(num+1);
+    }
+
+    function dec(){
+        setNum(num -1);
+    }
+
     return (
         <div className='UseStateHooks'>
-            <h1 className="heading">1</h1>
+            <h1 className="heading">{num}</h1>
             <div className="buttonSection">
-                <button className="btn">Increment</button>
-                <button className="btn">Decrement</button>
+                <button className="btn" onClick={inc}>Increment</button>
+                <button className="btn" onClick={dec}>Decrement</button>
             </div>
         </div>
 
