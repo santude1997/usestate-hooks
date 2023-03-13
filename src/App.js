4d4f1852f4515414f54b694763dfbc544f1b7f30
Santu De <Santu.De@inindcrws.com>
2023-03-13 16:38:33 +0530

    added nessary filr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Footer from './components/common/Footer/Footer';
+import Header from './components/common/Header/Header';
+import UseStateHooks from './components/UseStateHooks/UseStateHooks';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className='UseStateHooks'>
+      <UseStateHooks />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/UseStateHooks/UseStateHooks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const UseStateHooks = () => {
+    return (
+        <div className='UseStateHooks'>
+            <h1 className="heading">1</h1>
+            <div className="buttonSection">
+                <button className="btn">Increment</button>
+                <button className="btn">Decrement</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default UseStateHooks
